74feece2dcf4bac33f9fe36f402ec398c58a8ff8
unknown <juani.mp1@gmail.com>
2020-09-10 19:03:26 -0300

    coloreo

--- TP1/Calculadora/MiCalculadora/Form1.Designer.cs
+++ TP1/Calculadora/MiCalculadora/Form1.Designer.cs
@@ -48,27 +48,33 @@ namespace MiCalculadora
             // 
             // txtNumero1
             // 
-            this.txtNumero1.BackColor = System.Drawing.Color.White;
+            this.txtNumero1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(166)))), ((int)(((byte)(179)))));
+            this.txtNumero1.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.txtNumero1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumero1.ForeColor = System.Drawing.Color.White;
             this.txtNumero1.Location = new System.Drawing.Point(47, 100);
             this.txtNumero1.Name = "txtNumero1";
-            this.txtNumero1.Size = new System.Drawing.Size(183, 49);
+            this.txtNumero1.Size = new System.Drawing.Size(183, 42);
             this.txtNumero1.TabIndex = 0;
             this.txtNumero1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // txtNumero2
             // 
+            this.txtNumero2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(166)))), ((int)(((byte)(179)))));
+            this.txtNumero2.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.txtNumero2.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtNumero2.Location = new System.Drawing.Point(405, 99);
+            this.txtNumero2.ForeColor = System.Drawing.Color.White;
+            this.txtNumero2.Location = new System.Drawing.Point(405, 98);
             this.txtNumero2.Name = "txtNumero2";
-            this.txtNumero2.Size = new System.Drawing.Size(183, 49);
+            this.txtNumero2.Size = new System.Drawing.Size(183, 42);
             this.txtNumero2.TabIndex = 3;
             this.txtNumero2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // cmbOperador
             // 
+            this.cmbOperador.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(149)))), ((int)(((byte)(175)))), ((int)(((byte)(192)))));
             this.cmbOperador.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.cmbOperador.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbOperador.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cmbOperador.FormattingEnabled = true;
             this.cmbOperador.Items.AddRange(new object[] {
             " ",
@@ -78,76 +84,92 @@ namespace MiCalculadora
             "*"});
             this.cmbOperador.Location = new System.Drawing.Point(262, 99);
             this.cmbOperador.Name = "cmbOperador";
-            this.cmbOperador.Size = new System.Drawing.Size(113, 50);
+            this.cmbOperador.Size = new System.Drawing.Size(113, 41);
             this.cmbOperador.TabIndex = 2;
             // 
             // btnOperar
             // 
             this.btnOperar.AllowDrop = true;
             this.btnOperar.AutoEllipsis = true;
+            this.btnOperar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(166)))), ((int)(((byte)(179)))));
+            this.btnOperar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnOperar.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnOperar.Location = new System.Drawing.Point(47, 215);
+            this.btnOperar.Margin = new System.Windows.Forms.Padding(0);
             this.btnOperar.Name = "btnOperar";
             this.btnOperar.Size = new System.Drawing.Size(159, 41);
             this.btnOperar.TabIndex = 5;
             this.btnOperar.Text = "Operar";
-            this.btnOperar.UseVisualStyleBackColor = true;
+            this.btnOperar.UseVisualStyleBackColor = false;
             this.btnOperar.Click += new System.EventHandler(this.btnOperar_Click);
             // 
             // btnLimpiar
             // 
             this.btnLimpiar.AllowDrop = true;
             this.btnLimpiar.AutoEllipsis = true;
+            this.btnLimpiar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(166)))), ((int)(((byte)(179)))));
+            this.btnLimpiar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLimpiar.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F);
             this.btnLimpiar.Location = new System.Drawing.Point(238, 215);
             this.btnLimpiar.Name = "btnLimpiar";
             this.btnLimpiar.Size = new System.Drawing.Size(163, 41);
             this.btnLimpiar.TabIndex = 6;
             this.btnLimpiar.Text = "Limpiar";
-            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.UseVisualStyleBackColor = false;
             this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
             // 
             // btnCerrar
             // 
             this.btnCerrar.AllowDrop = true;
             this.btnCerrar.AutoEllipsis = true;
+            this.btnCerrar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(166)))), ((int)(((byte)(179)))));
+            this.btnCerrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCerrar.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F);
             this.btnCerrar.Location = new System.Drawing.Point(433, 215);
             this.btnCerrar.Name = "btnCerrar";
             this.btnCerrar.Size = new System.Drawing.Size(155, 41);
             this.btnCerrar.TabIndex = 7;
             this.btnCerrar.Text = "Cerrar";
-            this.btnCerrar.UseVisualStyleBackColor = true;
+            this.btnCerrar.UseVisualStyleBackColor = false;
             this.btnCerrar.Click += new System.EventHandler(this.btnCerrar_Click);
             // 
             // btnConvertirABinario
             // 
             this.btnConvertirABinario.AllowDrop = true;
             this.btnConvertirABinario.AutoEllipsis = true;
+            this.btnConvertirABinario.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(166)))), ((int)(((byte)(179)))));
+            this.btnConvertirABinario.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnConvertirABinario.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F);
             this.btnConvertirABinario.Location = new System.Drawing.Point(47, 289);
             this.btnConvertirABinario.Name = "btnConvertirABinario";
             this.btnConvertirABinario.Size = new System.Drawing.Size(243, 41);
             this.btnConvertirABinario.TabIndex = 8;
             this.btnConvertirABinario.Text = "Convertir a Binario";
-            this.btnConvertirABinario.UseVisualStyleBackColor = true;
+            this.btnConvertirABinario.UseVisualStyleBackColor = false;
             this.btnConvertirABinario.Click += new System.EventHandler(this.btnConvertirABinario_Click);
             // 
             // btnConvertirADecimal
             // 
             this.btnConvertirADecimal.AllowDrop = true;
             this.btnConvertirADecimal.AutoEllipsis = true;
+            this.btnConvertirADecimal.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(166)))), ((int)(((byte)(179)))));
+            this.btnConvertirADecimal.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnConvertirADecimal.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F);
             this.btnConvertirADecimal.Location = new System.Drawing.Point(345, 289);
             this.btnConvertirADecimal.Name = "btnConvertirADecimal";
             this.btnConvertirADecimal.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
             this.btnConvertirADecimal.Size = new System.Drawing.Size(243, 41);
             this.btnConvertirADecimal.TabIndex = 9;
             this.btnConvertirADecimal.Text = "Convertir a Decimal";
-            this.btnConvertirADecimal.UseVisualStyleBackColor = true;
+            this.btnConvertirADecimal.UseVisualStyleBackColor = false;
             this.btnConvertirADecimal.Click += new System.EventHandler(this.btnConvertirADecimal_Click);
             // 
             // lblResultado
             // 
-            this.lblResultado.BackColor = System.Drawing.Color.LightGray;
+            this.lblResultado.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
             this.lblResultado.CausesValidation = false;
             this.lblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblResultado.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblResultado.ForeColor = System.Drawing.Color.White;
             this.lblResultado.Location = new System.Drawing.Point(262, 28);
             this.lblResultado.Name = "lblResultado";
             this.lblResultado.Size = new System.Drawing.Size(326, 42);
@@ -158,7 +180,7 @@ namespace MiCalculadora
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.White;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
             this.ClientSize = new System.Drawing.Size(634, 361);
             this.Controls.Add(this.lblResultado);
             this.Controls.Add(this.btnConvertirADecimal);
@@ -169,8 +191,9 @@ namespace MiCalculadora
             this.Controls.Add(this.cmbOperador);
             this.Controls.Add(this.txtNumero2);
             this.Controls.Add(this.txtNumero1);
+            this.ForeColor = System.Drawing.SystemColors.Control;
             this.Name = "FormCalculadora";
-            this.Opacity = 0.9D;
+            this.Opacity = 0.99D;
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
             this.Text = "Calculadora de Mazzucchelli Juan del curso 2°A";
             this.ResumeLayout(false);
